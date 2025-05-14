9b9bc6f86d07be47ef265b5cfea186f9b26ca8e7
dimitriribeiro <dimitriiff00@gmail.com>
2025-05-14 13:48:38 -0300

    Criando a controller [Aula2]

---
+++ src/main/java/dev/Java10x/CadastroDeNinja/Controller.java
@@ -0,0 +1,15 @@
+package dev.Java10x.CadastroDeNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas") //pega informações
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
